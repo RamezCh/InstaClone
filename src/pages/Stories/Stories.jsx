c4d1e2574a80eb37811bed1d5a245c368a4162e7
RamezCh <ramiz_chreide@outlook.com>
2024-08-27 09:21:19 +0300

    Add Stories

--- src/pages/Stories/Stories.jsx
+++ src/pages/Stories/Stories.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import StoryImg from '../../assets/Temple_mount.jpg';
 import storiesData from './StoriesData';
+import TextEllipse from '../TextEllipse/TextEllipse';
 
 const Stories = () => {
   return (
@@ -19,6 +20,7 @@ const Stories = () => {
               className="rounded-full w-full h-full object-cover p-[2.5px] bg-black"
             />
           </div>
+          <TextEllipse username="Free Palestine" />
         </Link>
         {storiesData.map(story => {
           return (
@@ -27,13 +29,14 @@ const Stories = () => {
               key={story.id}
               className="flex items-center justify-center flex-col flex-shrink-0"
             >
-              <div className="w-16 h-16 rounded-full object-cover p-[2px] bg-green-600">
+              <div className="w-16 h-16 rounded-full object-cover p-[2px] bg-gradient-to-r from-[#f02aa6] to-[#ff6f48]">
                 <img
                   src={story.imageUrl}
                   alt="story img"
                   className="rounded-full w-full h-full object-cover p-[2.5px] bg-black"
                 />
               </div>
+              <TextEllipse username={story.username} />
             </Link>
           );
         })}
